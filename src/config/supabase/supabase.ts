3c462d44106822de319bb92cb8d60005f7a321aa
Radek <radek42310o@gmail.com>
2023-08-09 19:17:42 +0200

    fix: use env variables instead of config.json

---
+++ src/config/supabase/supabase.ts
@@ -0,0 +1,5 @@
+const DATABASE_URL = 'https://mamtaykanogpuzgxffxc.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hbXRheWthbm9ncHV6Z3hmZnhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTEyNTA1NTgsImV4cCI6MjAwNjgyNjU1OH0.DQxsPWxV7LQyuHyIV7JbVOU3FuMwAFwrbh50FH_b7Ks';
+
+export { DATABASE_URL, SUPABASE_KEY };
--- src/supabase.ts
+++ src/supabase.ts
@@ -1,5 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-import { DATABASE_URL, SUPABASE_KEY } from '@/config/supabase/supabase';
 import { Database } from '@/types/supabase/schema';
 
-export const supabase = createClient<Database>(DATABASE_URL, SUPABASE_KEY);
+export const supabase = createClient<Database>(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY);
