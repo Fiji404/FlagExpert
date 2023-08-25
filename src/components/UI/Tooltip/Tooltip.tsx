f1a4000a5f6c929e3fef2b320fae3d707e8f72d7
Radek <radek42310o@gmail.com>
2023-08-25 16:51:26 +0200

    fix: change tooltip font weight, remove unecessary animation

--- src/components/UI/Tooltip/Tooltip.tsx
+++ src/components/UI/Tooltip/Tooltip.tsx
@@ -14,7 +14,7 @@ export const Tooltip = ({ children, tooltipContent, delay }: Props) => {
                 <RadixTooltip.Trigger asChild>{children}</RadixTooltip.Trigger>
                 <RadixTooltip.Portal>
                     <RadixTooltip.Content
-                        className="data-[state=closed]:aniamte-[fadeOut_1s] z-20 rounded-md border border-[#e0e0e0] bg-[#f3f3f3] px-2 py-[1px] text-sm dark:border-[#2e2e2e] dark:bg-[#222222] dark:text-white"
+                        className="z-20 rounded-md border border-[#e0e0e0] bg-[#f3f3f3] px-2 py-[1px] text-sm font-medium dark:border-[#2e2e2e] dark:bg-[#222222] dark:text-white"
                         sideOffset={5}
                         asChild
                     >
