019f8d66be7a51ee5379f145a3922a9d178a6dd3
Fiji404 <radek42310o@gmail.com>
2023-10-09 17:35:07 +0200

    fix: add light theme for email confirm modal

--- src/components/UI/Backdrop.tsx
+++ src/components/UI/Backdrop.tsx
@@ -3,5 +3,5 @@ interface Props {
 }
 
 export const Backdrop = ({ onClick }: Props) => {
-    return <div onClick={onClick} className="fixed inset-0 z-10 bg-[rgba(0,0,0,0.6)]" aria-hidden="true"></div>;
+    return <div onClick={onClick} className="fixed inset-0 z-10 bg-[rgba(0,0,0,0.4)] dark:bg-[rgba(0,0,0,0.6)]" aria-hidden="true"></div>;
 };
--- src/pages/Auth/SignUp/SignUpConfirmationModal.tsx
+++ src/pages/Auth/SignUp/SignUpConfirmationModal.tsx
@@ -18,21 +18,23 @@ export const SignUpConfirmationModal = ({ closeConfirmationModal, emailToConfirm
     return (
         <>
             <Backdrop onClick={closeConfirmationModal} />
-            <div className="modal-shadow fixed left-1/2 top-1/2 z-20 w-[90%] max-w-3xl -translate-x-1/2 -translate-y-1/2 rounded-lg bg-[#18191b] p-3">
+            <div className="modal-shadow dark:modal-shadow-dark fixed left-1/2 top-1/2 z-20 w-[90%] max-w-3xl -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white p-3 dark:bg-[#18191b]">
                 <button
                     onClick={closeConfirmationModal}
-                    className="ml-auto block rounded-full transition-colors hover:bg-[#34363a]"
+                    className="ml-auto block rounded-full transition-colors hover:bg-[#dfdfdf] dark:hover:bg-[#34363a]"
                 >
-                    <IoMdClose className=" text-3xl text-white" />
+                    <IoMdClose className=" text-3xl text-black dark:text-white" />
                 </button>
-                <IoIosMail className="mx-auto text-9xl text-[#5ce96f]" />
-                <h2 className="text-center text-4xl font-semibold text-white">Email confirmation</h2>
-                <p className="mt-4 text-center text-[#ccc]">
-                    We have sent email to <span className="text-[#5ce96f]">{emailToConfirm}</span> {false} to confirm
+                <IoIosMail className="mx-auto text-9xl dark:text-[#5ce96f] text-[#4dd15f]" />
+                <h2 className="text-center text-4xl font-semibold text-[#000000] dark:text-white">
+                    Email confirmation
+                </h2>
+                <p className="mt-4 text-center text-[#4b4b4b] dark:text-[#ccc]">
+                    We have sent email to <span className="dark:text-[#5ce96f] text-[#4dd15f]">{emailToConfirm}</span> {false} to confirm
                     the validity of your email address. After recieving the email follow the link provided to complete
                     registration
                 </p>
-                <p className="mt-5 border-t border-[#3b3b3b] pt-3 text-center text-[#ccc]">
+                <p className="mt-5 border-t dark:border-[#3b3b3b] border-[#cfcfcf] pt-3 text-center dark:text-[#ccc] text-[#4b4b4b]">
                     If you not get any confirmation email{' '}
                     <button onClick={resendEmailConfirmation} className="text-[#1276E2] hover:underline">
                         Resend confirmation email
